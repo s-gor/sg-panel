86f2cd974476a11a86f8b446f0e0bc8aa485f6d8
Ser.Gor <infosec.co.il@gmail.com>
2026-06-26 08:14:25 +0300

    SG-Panel v0.9.8: safe uninstall

--- install-or-upgrade.sh
+++ install-or-upgrade.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-EXPECTED_VERSION="0.9.7"
+EXPECTED_VERSION="0.9.8"
 TARGET="/opt/xpanel-mvp"
 SERVICE="xpanel-web"
 SOURCE_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
